5c9fe82cc1c992d9df6feaecacc9891325366b25
Akhgofur <gofurbekaxmadjonov397@gmail.com>
2023-10-28 11:06:48 +0300

    check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,22 @@
-
-import Loading from '@/components/loading'
+import Loading from "@/components/loading";
 // import Layout from '../layout'
 // import HomeInner from '../widgets'
-import { Suspense, useEffect, useState } from 'react'
-import dynamic from 'next/dynamic'
+import { Suspense, useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 
-const HomeInner = dynamic(() => import("@/widgets"), {ssr: false})
-const Layout = dynamic(() => import("@/layout"), {ssr: false})
-
+const HomeInner = dynamic(() => import("@/widgets"), { ssr: false });
+const Layout = dynamic(() => import("@/layout"), { ssr: false });
 
 const Home = () => {
-
-
-
-  
   return (
-   
     <>
-    
-    <Suspense fallback={<Loading />} >
-    <Layout>
-      <HomeInner />
-   </Layout>
-    </Suspense>
+      <Suspense fallback={<Loading />}>
+        <Layout>
+          <HomeInner />
+        </Layout>
+      </Suspense>
     </>
-  )
-}
+  );
+};
 
-
-export default Home
+export default Home;
--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,12 +1,20 @@
-import Layout from "@/layout"
-import NewsInner from "@/widgets/news"
+import Loading from "@/components/loading";
+// import  from "@/layout";
+// import  from "@/widgets/news";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
+const NewsInner = dynamic(() => import("@/widgets/news"))
+const  Layout = dynamic(() => import("@/layout"))
 
 const News = () => {
-    return (
-        <Layout>
-            <NewsInner />
-        </Layout>
-    )
-}
+  return (
+    <Suspense fallback={<Loading />}>
+      <Layout>
+        <NewsInner />
+      </Layout>
+    </Suspense>
+  );
+};
 
-export default News
+export default News;
